19a77e60250abfd38ff1707e85bd80d5c977a360
Erik Price <github@erikprice.net>
2015-02-26 17:45:54 -0600

    Add stupidly simple proof of concept test

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,39 @@
+#![feature(libc)]
+#![allow(dead_code)]
+
 #[macro_use]
 extern crate syscall;
 extern crate libc;
-extern crate alloc;
 
-mod thread;
-mod futex;
-mod clone;
+//mod thread;
+pub mod futex;
+pub mod clone;
 
 #[test]
 fn it_works() {
+    use libc;
+    use std::ptr;
+    use std::mem;
+    use std::old_io::timer;
+    use std::time::duration::Duration;
+
+    extern fn test(_: *mut libc::c_void) -> libc::c_int {
+        println!("Hello! I am a thread");
+        0
+    }
+
+    let id = unsafe {
+        let stack = libc::malloc(1024 * 1024);
+
+        clone::clone(test,
+                     std::mem::transmute(stack.offset(1024 * 1024)),
+                     clone::VM | clone::FILES | clone::FS |
+                     clone::SIGHAND | clone::DETACHED,
+                     ptr::null_mut())
+    };
+    println!("child id = {}", id);
+
+    timer::sleep(Duration::seconds(2));
+
+    println!("And I'm the parent!");
 }
