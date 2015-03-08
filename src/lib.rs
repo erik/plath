aa4e6c4fe8eb57c042e87c8daccc4231a974ab39
Erik Price <github@erikprice.net>
2015-03-07 20:07:28 -0600

    Make sure code only works on supported platform

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
 #![feature(asm, core, libc)]
 #![allow(dead_code, raw_pointer_derive)]
 
+// We only work on 64 bit linux
+#![cfg(all(target_os = "linux", target_pointer_width = "64"))]
+
 #[macro_use]
 extern crate syscall;
 extern crate libc;
